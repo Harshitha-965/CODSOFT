4e68db9a9a03e866c03fa1a26c460d8b8e6b7ba2
Harshitha-965 <harshithakg09@gmail.com>
2024-07-05 19:37:01 +0530

    commit

--- Lvl1-Task3/script.js
+++ Lvl1-Task3/script.js
@@ -1,1 +1,21 @@
+const display=document.querySelector(.display);
+const buttons=document.querySelectorAll(buttons));
+const specialChars=["%","*","/","-","+","="];
+let output="";
+
+const calculate=(btnValue)=>{
+    if(btnValue === "=" && output!==""){
+        output=eval(output.replace("%","/100"));
+    }
+    else if(btnValue==="AC"){
+        output="";
+    }
+    else if(btnValue==="DEL"){
+        output=output.toString().slice(0,-1);
+    }
+    else{
+        if(output==="" && specialChars.includes(btnValue)) return;
+    }
+    display.value=output;
+}
 
